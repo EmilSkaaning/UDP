45078d520857d67300be1f419fad216ee3c0943e
EmilSkaaning <thygesen1212@gmail.com>
2018-12-03 16:32:31 +0100

    FUCKING WORKING

--- ecg.c
+++ ecg.c
@@ -47,10 +47,8 @@ int main(){
 		char *hex_id = (char*) malloc(1);
 		char *hex_flag = (char*) malloc(1);
         char *ph = (char*) malloc(1);
-		int checksum, id, flag;
+		int checksum, id, flag, flag_rec, flag_data;
 		while(1){
-
-			memset((char *) &data_s, 0 , FRAME_PAYLOAD_SIZE);
 
 			switch(sender){
 				case 'i':  // Idle
@@ -108,6 +106,35 @@ int main(){
 						return 1;
 	     		    }
 	     		    printf("send data: %s\n", data_s);
+	
+			        sender = 'w';
+			        break;
+/*	     		    
+		            if ( (len=radio_recv(&source, buf_s, TIMEOUT_SEC * 1000)) < 0) {
+			            if (len == ERR_TIMEOUT) {
+			                printf("radio_recv timed out\n");
+			                continue;
+			            }
+		            printf("radio_recv failed with %d\n", len);
+		            return 1;
+	       			}			 		
+
+					printf("Buf_s: %s \n", buf_s);
+
+			        strncpy(ph, buf_s+39, 1);  
+			        flag = atoi(ph);      		 
+
+			        if (flag == 4){
+			        	sender ='d';
+			        	printf("Ack recieved sending data\n");
+			        }
+			        else{
+			        	break;
+			        }
+*/
+				case 'd':
+					printf("case (send): send payload\n");
+
 					memset((char *) &data_s, 0 , FRAME_PAYLOAD_SIZE);
 					memset((char *) &header, 0 , 13);
 					// preamble for the data_sframe
@@ -134,8 +161,21 @@ int main(){
 	     		    
 			        strcat(data_s, header);
 			        strcat(data_s, payload);
-			        printf("data_s: %s\n", data_s);	
-	     		    
+			        //printf("data_s: %s\n", data_s);
+
+			        // Send pakage
+			        if ( (err=radio_send(source, data_s, FRAME_PAYLOAD_SIZE)) != ERR_OK) {
+			            printf("radio_send failed with %d\n", err);
+						return 1;
+	     		    }
+
+					printf("sending data: %s\n", data_s);
+
+					sender = 'w';  // Going back to idle			        
+					break;
+
+				case 'w':
+					printf("case (send): waiting for ack\n");
 		            if ( (len=radio_recv(&source, buf_s, TIMEOUT_SEC * 1000)) < 0) {
 			            if (len == ERR_TIMEOUT) {
 			                printf("radio_recv timed out\n");
@@ -148,34 +188,70 @@ int main(){
 					printf("Buf_s: %s \n", buf_s);
 
 			        strncpy(ph, buf_s+39, 1);  
-			        flag = atoi(ph);      		 
+			        flag_rec = atoi(ph);      		 
 
-			        if (flag == 4){
+			        printf("flag: %d, flag_rec: %d, flag_data: %d \n", flag, flag_rec, flag_data);
+			        if (flag == 8 && flag_rec == 4){
 			        	sender ='d';
 			        	printf("Ack recieved sending data\n");
 			        }
-			        else{
-			        	break;
+			        else if (flag == 0 && flag_rec == 4){
+			     		sender = 't';   	
+			     		printf("Ack recieved terminating\n");
 			        }
+			        else if (flag == 2 && flag_rec == 4){
+			        	sender = 'i'; 					
+			        }	
+			        else{
+			        	printf("%s\n", "Should not happen");
+			        	sender = 'd';
+			        }		        
+					break;
 
-				case 'd':
-					printf("case (send): send payload\n");
+				case 't':
+					printf("case (send): terminating, waiting for ack\n");
+					memset((char *) &data_s, 0 , FRAME_PAYLOAD_SIZE);
+					// preamble for the data_sframe
 
-			        // Send pakage
-			        if ( (err=radio_send(source, data_s, FRAME_PAYLOAD_SIZE)) != ERR_OK) {
+				    pre = "AAAAAAAAAAAAAAAAAAAA";
+				    strcpy(data_s, pre);
+			   
+
+			        magickey = "1337F542";
+			        strcat(data_s, magickey);
+			   
+			        // Header
+
+			        sprintf(header, "%02lx", strlen(payload));
+			        // printf("header: %s\n", header);
+			   
+
+			        checksum = 0;
+			        sprintf(hex_sum, "%08x", checksum);
+			        strcat(header, hex_sum);
+			        // printf("checksum: %s\n", header);
+			   
+			        id = 1;
+			        sprintf(hex_id, "%01x", id);
+			        strcat(header, hex_id);
+			        // printf("id: %s\n", header);
+
+			   
+			        flag = 2;
+			        sprintf(hex_flag, "%01x", flag);
+			        strcat(header, hex_flag);
+			        // printf("flag: %s\n", header);
+			   
+			        strcat(data_s, header);
+			   
+			        if ( (err=radio_send(rcv_addr, data_s, FRAME_PAYLOAD_SIZE)) != ERR_OK) {
 			            printf("radio_send failed with %d\n", err);
 						return 1;
 	     		    }
 
-					printf("sending data: %s\n", data_s);
-
-					sender = 'i';  // Going back to idle			        
+	     		    sender = 'i';
 					break;
-
-	/*			case 'done':
-				case 'end':
-					break;
-	*/		}
+			}
 		}
 	}
 
@@ -184,7 +260,7 @@ int main(){
 		char reciever = 'r';
 	    char buf_r[FRAME_PAYLOAD_SIZE + 1];
  		char data_r[FRAME_PAYLOAD_SIZE];
- 		int err, len, source, flag_int, checksum_int, id_int;
+ 		int err, len, source, flag_int, flag_rec, checksum_int, id_int;
         char *preamble = (char*) malloc(20); 
         char *magickey = (char*) malloc(8);
 		char *magickey_data = (char*) malloc(8);  
@@ -242,11 +318,10 @@ int main(){
 			        strncpy(flag, header+11, 1);        
 			        // printf("Received flag: %s\n", flag);        
 
-			        flag_int = atoi(flag);
+			        flag_rec = atoi(flag);
 
-			        if (flag_int == 8){
+			        if (flag_rec == 8 || flag_rec == 2){
 			        	reciever = 'a';
-			        	printf("reciever set to: %c\n", reciever);
 			        }
 			        else{
 			        	reciever = 'r';
@@ -280,7 +355,19 @@ int main(){
 			        sprintf(hex_id, "%01x", id_int);
 			        strcat(header, hex_id);
 			        // printf("id: %s\n", header);
-
+			        printf("flag_rec %d\n", flag_rec);
+			        if (flag_rec == 8){
+	     		   		reciever = 'd';
+			        }
+			        else if (flag_rec == 2){
+			        	reciever = 't';
+			        }
+			        else if (flag_rec == 0){
+			        	reciever = 'w';
+			        }
+			        else{
+			        	reciever = 'r';
+			        }
 
 			        flag_int = 4;
 			        sprintf(hex_flag, "%01x", flag_int);
@@ -293,9 +380,7 @@ int main(){
 			            printf("radio_send failed with %d\n", err);
 						return 1;
 	     		    }		
-
-	     		    reciever = 'd';
-
+	     		    printf("Sending ack: %s\n", data_r);
 	     		    break;
 				case 'd':
 
@@ -334,15 +419,69 @@ int main(){
 
 			        strncpy(flag, header+11, 1);        
 			        // printf("Received flag: %s\n", flag);       
+			        flag_rec = atoi(flag);
 
 					strncpy(pakage, buf_r+40, 144);
-
-					if (atoi(pay_len) == strlen(pakage)){
+					printf("flag_rec: %d\n", flag_rec);
+					if (atoi(pay_len) == strlen(pakage) && flag_rec == 0){
 						printf("All data recieved: %s\n", pakage);
 						reciever = 'a';
 					}
+					else if (atoi(pay_len) == strlen(pakage) && flag_rec == 2){
+						printf("All data recieved: %s\n", pakage);
+						reciever = 't';
+					}
 
 					break;
+				case 'w':
+					printf("%s\n", "Waiting for termination");
+
+					memset((char *) buf_r, 0, FRAME_PAYLOAD_SIZE+1);
+					printf("case (rec): recieve data\n");
+		            if ( (len=radio_recv(&source, buf_r, TIMEOUT_SEC * 1000)) < 0) {
+			            if (len == ERR_TIMEOUT) {
+			                printf("radio_recv timed out\n");
+			                continue;
+			            }
+		            printf("radio_recv failed with %d\n", len);
+		            return 1;
+	       			}					
+
+			        strncpy(preamble, buf_r, 20);
+			        // printf("Received preamble (hex): %s\n", preamble);
+
+      				strncpy(magickey_data, buf_r+20, 8);
+
+			        // printf("Received magickey (hex): %s\n", magickey);
+
+			        strncpy(header, buf_r+28, 12);
+			        // printf("Received header (hex): %s\n", header);
+
+			        strncpy(pay_len, header, 2);
+			        // printf("Received pay_len : %s\n", pay_len);
+
+			        strncpy(checksum, header+2, 8);
+			        // printf("Received checksum (hex): %s\n", checksum);
+
+			        strncpy(id, header+10, 1);
+			        // printf("Received id: %s\n", id);
+
+			        strncpy(flag, header+11, 1);
+			        flag_rec = atoi(flag);        
+			        // printf("Received flag: %s\n", flag); 
+
+			        if (flag_rec == 2){
+			        	reciever = 'a';
+			        }
+			        else{
+			        	printf("It did not terminate! flag_rec should be 2 but is %d\n", flag_rec);
+			        }
+
+				case 't':
+					printf("%s\n", "Det var det ");
+					reciever = 'r';
+					break;
+
 	
 			}
 		}
